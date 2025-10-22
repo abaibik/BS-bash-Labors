55c77878fe7d3e84b135f4eaeac9c92879d7f790
Alexandra Baibik <tetatinang@Alexandras-MacBook-Air.local>
2025-10-22 09:17:24 +0200

    add skeleton code

---
+++ du_statistics.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+#######################################################################
+# Sample solution for Lab 1 exercise "du_statistics.sh"
+# Usage:
+#   du_statistics.sh [--count|--size]
+# The default --count shows the total number of users, while
+# The option --size shows the size of each individual user directory (in megabytes).
+#
+# Author: Aleksandra Baibik
+# Date: 2025-10-22
+#######################################################################
+
+function show_help {
+    echo "Usage: du_statistics.sh [-h|--help] [-c|--count] [-s|--size]"
+    echo ""
+    echo "Analyses the user quantity and the size of each user's home directory, as well as the total storage space for each organization."
+    echo ""
+    echo "  -h|--help:  Show this help and quit"
+    echo "  -c|--count: Shows the total number of users"
+    echo "  -s|--size:  Shows size of each individual user directory (in megabytes)"
+    echo ""
+}
+
+function show_count {
+    echo "Under construction"
+}
+
+function show_size {
+    echo "Under construction"
+}
+
+while test $# -gt 0 ; do
+    case "$1" in
+    -h|--help)
+        show_help
+        exit
+        ;;
+    -c|--count)
+        show_count
+        exit
+        ;;
+    -s|--size)
+        show_size
+        exit
+        ;;
+    esac
+    shift
+done
+
+# If nothing has been selected, count is the default
+show_count
